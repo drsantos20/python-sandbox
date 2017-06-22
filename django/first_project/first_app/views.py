9c3c58dd888b1be71410caf732a2a0105d57cb1c
drsantos <drsantos20@gmail.com>
2017-06-21 21:23:55 -0300

    django full stack sand-box

---
+++ django/first_project/first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    my_dict = {'insert_me':"Hello I am from views.py! on templates/first_app"}
+    return render(request, 'first_app/index.html', context=my_dict)
+
+# Create your views here.
